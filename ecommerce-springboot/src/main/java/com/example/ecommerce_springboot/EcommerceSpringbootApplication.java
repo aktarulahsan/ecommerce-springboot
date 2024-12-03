6966f8289958c9d3f17651c0525c321700a580b7
aktarul ahsan <md.aktarulahsan@gmaill.com>
2024-12-03 10:34:40 +0600

    swagger add

--- ecommerce-springboot/src/main/java/com/example/ecommerce_springboot/EcommerceSpringbootApplication.java
+++ ecommerce-springboot/src/main/java/com/example/ecommerce_springboot/EcommerceSpringbootApplication.java
@@ -2,14 +2,25 @@ package com.example.ecommerce_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
+
 @SpringBootApplication
-public class EcommerceSpringbootApplication {
+public class EcommerceSpringbootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceSpringbootApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EcommerceSpringbootApplication.class);
+	}
+
 
 }
